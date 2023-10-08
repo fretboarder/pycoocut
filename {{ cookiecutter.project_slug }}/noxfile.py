d4a14ea9bce35b1cdd681fbbf1225d36a19a2ab4
fretboarder <cini01@gmail.com>
2023-10-08 12:12:59 +0200

    fix: update cookiecutter

--- {{ cookiecutter.project_slug }}/noxfile.py
+++ {{ cookiecutter.project_slug }}/noxfile.py
@@ -8,6 +8,13 @@ def ruff(session: nox.Session):
     session.run("ruff", "check", "{{cookiecutter.project_slug}}")
 
 
+@nox.session
+def mypy(session: nox.Session):
+    """Running ruff"""
+    session.install("ruff", "click")
+    session.run("mypy", "{{cookiecutter.project_slug}}")
+
+
 @nox.session()
 def pytest(session: nox.Session):
     """Running pytest"""
